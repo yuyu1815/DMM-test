5ef648741002ee5fe92584499c12debff62bf60f
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2024-11-02 15:45:39 +0900

    課題提出用

---
+++ h2-1/js/alert.js
@@ -0,0 +1,10 @@
+function loadHello(){
+    alert("Hello!");
+}
+
+function sum(){
+    let x = 3;
+    let y = 4;
+    let z = x + y;
+    alert(z);
+}
---
+++ h2-1/js/omikuji.js
@@ -0,0 +1,24 @@
+let name;
+let username;
+let userresult;
+
+username = prompt("お名前を教えて下さい。");
+document.getElementById("name").innerHTML = username;
+
+let rand = Math.floor( Math.random() * 5);
+if (rand === 0) {
+    userresult = "大吉";
+}else if (rand === 1) {
+    userresult = "中吉";
+}else if (rand === 2) {
+    userresult = "小吉";
+}else if (rand === 3) {
+    userresult = "吉";
+}else if (rand === 4) {
+    userresult = "凶";
+}
+/*
+    len unsei =["大吉","中吉","小吉","吉","凶"]
+    userresult = unsei[rand];
+ */
+document.getElementById("result").innerHTML = userresult;
