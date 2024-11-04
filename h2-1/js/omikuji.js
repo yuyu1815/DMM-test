1a19b99451e9314f4b1b93584e09c6db9663d59e
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2024-11-05 00:43:27 +0900

    [Update]不足分のコード追加

--- h2-1/js/omikuji.js
+++ h2-1/js/omikuji.js
@@ -3,7 +3,7 @@ let username;
 let userresult;
 
 username = prompt("お名前を教えて下さい。");
-document.getElementById("name").innerHTML = username;
+document.getElementById("name").innerHTML = username === "" ? "名無し" : username;
 
 let rand = Math.floor( Math.random() * 5);
 if (rand === 0) {
